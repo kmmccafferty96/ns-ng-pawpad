861641b131310e4e3a76c14deaa7cf87395267ea
Keegan McCafferty <kmmccafferty96@gmail.com>
2020-04-21 13:13:19 -0400

    chore: remove route tracing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,17 +8,17 @@ const routes: Routes = [
     { path: '', redirectTo: '/pages', pathMatch: 'full' },
     {
         path: 'auth',
-        loadChildren: () => import('~/app/auth/auth.module').then(m => m.AuthModule),
+        loadChildren: () => import('~/app/auth/auth.module').then((m) => m.AuthModule),
     },
     {
         path: 'pages',
-        loadChildren: () => import('~/app/pages/pages.module').then(m => m.PagesModule),
+        loadChildren: () => import('~/app/pages/pages.module').then((m) => m.PagesModule),
         //canLoad: [AuthGuard]
     },
 ];
 
 @NgModule({
-    imports: [NativeScriptRouterModule.forRoot(routes, { enableTracing: false })],
+    imports: [NativeScriptRouterModule.forRoot(routes)],
     exports: [NativeScriptRouterModule],
     providers: [AuthGuard],
 })
